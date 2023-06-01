b84ef2ba6752cc567c7a6cf00533b854f1351a09
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-06-02 00:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 function giveInput() {
-	let prom=new Promise((resolve)=>{
-		setTimeout(()=>{
-			resolve("Hello, world!");
+	let prom=new Promise((resolve) => {
+		setTimeout(() => {
+			resolve('Hello, world!');
 		},1000)
 	})
 	return prom;
 }
 let x=giveInput();
-x.then(data){
+x.then((data) => {
 	let div=document.getElementById("output");
 	div.innerHTML=data;
-}
+})
